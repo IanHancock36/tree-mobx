60c236835939f1674d7f7cd8a83c44d2acd1bcaf
Ian Hancock <northroadian@gmail.com>
2021-09-27 06:01:52 -0500

    fortune list

--- src/models/FortuneCookieList.js
+++ src/models/FortuneCookieList.js
@@ -1,5 +1,5 @@
 import { types } from 'mobx-state-tree'
-
+import Fortunes from './Fortunes'
 const fortuneCookieList = types.model("FortuneCookieList",{
     //fortunes is the object array from the api call. 
     fortunes: types.array(Fortunes)
--- src/models/Fortunes.js
+++ src/models/Fortunes.js
@@ -1,1 +1,10 @@
 import { types } from 'mobx-state-tree'
+
+const Fortune = types.model('Fortune',{
+    quantity: types.number,
+    id: types.string,
+    message: types.string, 
+})
+export default Fortune
+// the api call for this is 'http://fortunecookieapi.com/v1/fortunes?limit=&skip=&page='
+
